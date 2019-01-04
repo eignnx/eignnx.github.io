bd744b7cfe7a1b634441a563f86981495b88af2e
eignnx <eignnx@gmail.com>
2019-01-03 23:34:41 -0500

    initial commit for dom-tree project

---
+++ projects/dom-tree/app.js
@@ -0,0 +1,90 @@
+let ctx = {
+  selected: null
+}
+
+
+function applyMouseEvents(selected) {
+  selected
+    .mouseover(function(e) {
+      e.stopPropagation()
+      const parent = $(this).parent()
+      if (!parent.is('#tree-container')) {
+        parent.removeClass('tree-ele-highlight')
+        parent.addClass('tree-ele-no-highlight')
+      }
+      $(this).addClass('tree-ele-highlight')
+      $(this).removeClass('tree-ele-no-highlight')
+    })
+    .mouseout(function() {
+      $(this).addClass('tree-ele-no-highlight')
+      $(this).removeClass('tree-ele-highlight')
+    })
+    .click(function(e) {
+      e.stopPropagation()
+      if (ctx.selected) {
+        ctx.selected.removeClass('selected');
+      }
+      ctx.selected = $(this)
+      $(this).addClass('selected')
+    })
+}
+
+$(document).ready(() => {
+  $('html').click(function() {
+    if (ctx.selected) {
+      ctx.selected.removeClass('selected')
+    }
+    ctx.selected = null;
+  })
+
+  let root =
+    vertical([
+      horizontal([horizontal([square(), circle()])]),
+      circle()
+    ])
+})
+
+function atom() {
+  const dom = $(`<div>123</div>`)
+  dom.addClass('tree-ele-no-highlight atom')
+  applyMouseEvents(dom)
+  $('#tree-container').append(dom)
+  return dom
+}
+
+function square() {
+  const dom = atom()
+  dom.addClass('square')
+  dom.html('<span>sq</span>')
+  return dom
+}
+
+function circle() {
+  const dom = atom()
+  dom.addClass('circle')
+  dom.html('<span>cr</span>')
+  return dom
+}
+
+function composite(children, type) {
+  const dom = $(`<div></div>`)
+  dom.addClass('tree-ele-no-highlight')
+  dom.html(type)
+  dom.data('children', children)
+  children.forEach(child => dom.append(child))
+  applyMouseEvents(dom)
+  $('#tree-container').append(dom)
+  return dom
+}
+
+function vertical(children) {
+  const dom = composite(children, 'vertical')
+  dom.addClass('vertical')
+  return dom
+}
+
+function horizontal(children) {
+  const dom = composite(children, 'horizontal')
+  dom.addClass('horizontal')
+  return dom
+}
