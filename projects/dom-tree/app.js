cbeefb9796f1fe3aa0ebab6881f0fbc832d687c1
eignnx <eignnx@gmail.com>
2019-01-04 00:47:48 -0500

    Split dom element constructors into own file

--- projects/dom-tree/app.js
+++ projects/dom-tree/app.js
@@ -2,41 +2,6 @@ let ctx = {
   selected: null
 }
 
-
-function applyMouseEvents(selected) {
-  selected
-    .mouseover(function(e) {
-      e.stopPropagation()
-      const parent = $(this).parent()
-      if (!parent.is('#tree-container')) {
-        parent.removeClass('tree-ele-highlight')
-        parent.addClass('tree-ele-no-highlight')
-      }
-      $(this).addClass('tree-ele-highlight')
-      $(this).removeClass('tree-ele-no-highlight')
-    })
-    .mouseout(function() {
-      $(this).addClass('tree-ele-no-highlight')
-      $(this).removeClass('tree-ele-highlight')
-    })
-    .click(function(e) {
-      e.stopPropagation()
-
-      // Deselect if background is clicked, or if the selected item is clicked.
-      if (ctx.selected) {
-        if (ctx.selected.is($(this))) {
-          ctx.selected.removeClass('selected')
-          ctx.selected = null;
-        } else {
-          ctx.selected.removeClass('selected')
-          ctx.selected = $(this).addClass('selected')
-        }
-      } else {
-        ctx.selected = $(this).addClass('selected')
-      }
-    })
-}
-
 $(document).ready(() => {
   $('html').click(function() {
     if (ctx.selected) {
@@ -61,48 +26,3 @@ $(document).ready(() => {
       ])
     ])
 })
-
-function atom() {
-  const dom = $('<div>')
-  dom.addClass('tree-ele-no-highlight atom')
-  applyMouseEvents(dom)
-  $('#tree-container').append(dom)
-  return dom
-}
-
-function square() {
-  const dom = atom()
-  dom.addClass('square')
-  dom.html('<span>sq</span>')
-  return dom
-}
-
-function circle() {
-  const dom = atom()
-  dom.addClass('circle')
-  dom.html('<span>cr</span>')
-  return dom
-}
-
-function composite(children, type) {
-  const dom = $('<div>')
-  dom.addClass('tree-ele-no-highlight')
-  dom.html(type)
-  dom.data('children', children)
-  children.forEach(child => dom.append(child))
-  applyMouseEvents(dom)
-  $('#tree-container').append(dom)
-  return dom
-}
-
-function vertical(children) {
-  const dom = composite(children, 'vertical')
-  dom.addClass('vertical')
-  return dom
-}
-
-function horizontal(children) {
-  const dom = composite(children, 'horizontal')
-  dom.addClass('horizontal')
-  return dom
-}
